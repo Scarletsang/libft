07f69d03ca20ceade2dda8312745a7d9ad14ad0b
Anthony Tsang <anthonytsang0226@gmail.com>
2022-05-08 18:01:10 +0800

    fixed strstr to pass the libft unit test case 02

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,34 +6,35 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/08 15:36:37 by htsang            #+#    #+#             */
-/*   Updated: 2022/05/08 16:08:19 by htsang           ###   ########.fr       */
+/*   Updated: 2022/05/08 17:59:28 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+
 char	*ft_strstr(const char *str, const char *to_find)
 {
-	int		i;
 	int		j;
 	char	*p;
 
-	i = 0;
 	j = 0;
 	p = 0;
 	if (*to_find == 0)
 		return ((char *) str);
-	while (str[i] && to_find[j])
+	while (*(str + j) && to_find[j])
 	{
-		if (str[i++] == to_find[j])
+		if (*(str + j) == to_find[j])
 		{
 			if (p == 0)
-				p = (char *) &str[i - 1];
+				p = (char *) str;
 			j++;
 			continue ;
 		}
+		str++;
 		p = 0;
 		j = 0;
 	}
 	if (to_find[j] == 0)
 		return (p);
-	return (0);
+	return (NULL);
 }
