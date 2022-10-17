9836ec116f361dc88b0f493b27fb4e5fb99283bc
Hei yiu Tsang <htsang@2-E-2.42heilbronn.de>
2022-10-17 17:19:50 +0200

    added helper function ft_strdup in ft_strcpy

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,12 +6,27 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/08 22:06:49 by htsang            #+#    #+#             */
-/*   Updated: 2022/05/16 23:54:58 by htsang           ###   ########.fr       */
+/*   Updated: 2022/10/17 17:17:26 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
+
+char	*ft_strcpy(char *dest, const char *src)
+{
+	char	*tmp;
+
+	tmp = dest;
+	while (*src)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = 0;
+	return (tmp);
+}
 
 char	*ft_strdup(const char *str)
 {
