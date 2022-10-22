498ff8fbdc3498b8bf04fd58bca53468090f7a56
Hei yiu Tsang <htsang@1-C-10.42heilbronn.de>
2022-10-22 23:40:48 +0200

    added striteri strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/22 20:54:14 by htsang            #+#    #+#             */
+/*   Updated: 2022/10/22 22:53:41 by htsang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+/* Allocate memory of the result of a string
+that are mapped over by a function. */
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*result;
+
+	if (!s || !f)
+	{
+		return (NULL);
+	}
+	result = (char *) malloc(ft_strlen(s) + 1);
+	if (!result)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (s[i])
+	{
+		result[i] = f(i, s[i]);
+		i++;
+	}
+	result[i] = 0;
+	return (result);
+}
