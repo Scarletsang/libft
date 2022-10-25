eaad9ba64c9a48d9cb0a7cab95c21f29fb0a34dd
Hei yiu Tsang <htsang@2-I-4.42heilbronn.de>
2022-10-25 22:34:34 +0200

    fixed substr to allocate the correct memory
    it happens when max_size is bigger than the possible size

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 18:40:48 by htsang            #+#    #+#             */
-/*   Updated: 2022/10/19 18:23:12 by htsang           ###   ########.fr       */
+/*   Updated: 2022/10/25 22:27:46 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,23 @@ The substring starts at an index with a given length. */
 char	*ft_substr(char const *s, unsigned int start, size_t max_size)
 {
 	char	*ptr;
+	size_t	possible_size;
 
 	if (!s)
-	{
 		return (NULL);
-	}
-	ptr = (char *) malloc(max_size + 1);
-	if (!ptr)
-	{
-		return (NULL);
-	}
 	while (*s && start)
 	{
 		s++;
 		start--;
 	}
+	possible_size = 0;
+	while (s[possible_size])
+		possible_size++;
+	if (max_size > possible_size)
+		max_size = possible_size;
+	ptr = (char *) malloc(max_size + 1);
+	if (!ptr)
+		return (NULL);
 	ft_strlcpy(ptr, s, max_size + 1);
 	return (ptr);
 }
