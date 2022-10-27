5e4eca491012d1945aa62014ea6a6e7d7c4258d5
Anthony Tsang <anthonytsang0226@gmail.com>
2022-10-27 13:27:56 +0200

    removed variable p, cast void pointer in compile time

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 22:38:45 by htsang            #+#    #+#             */
-/*   Updated: 2022/10/27 12:58:17 by htsang           ###   ########.fr       */
+/*   Updated: 2022/10/27 13:25:20 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,10 @@
 /* Fill an array of n size with the same char. */
 void	*ft_memset(void *str, int c, size_t n)
 {
-	unsigned char	*p;
-
-	p = (unsigned char *) str;
 	while (n > 0)
 	{
-		*p = (unsigned char) c;
-		p++;
+		*((unsigned char *) str) = (unsigned char) c;
+		str++;
 		n--;
 	}
 	return (str);
