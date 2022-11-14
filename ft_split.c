c7e3e6463bdecf7268af8557990d8e8a387f8751
Anthony Tsang <anthonytsang0226@gmail.com>
2022-11-14 19:41:24 +0100

    fixed split's freeing size due to malloc failure

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 11:23:16 by htsang            #+#    #+#             */
-/*   Updated: 2022/11/07 15:08:23 by htsang           ###   ########.fr       */
+/*   Updated: 2022/11/14 19:40:59 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,12 +45,17 @@ static size_t	ft_count_word(char const *str, char c)
 /**
 ** @brief free pointers contained in an array
 */
-static char	*ft_deep_free(char **result, size_t count)
+static char	**ft_deep_free(char **result, size_t count)
 {
-	while (count)
+	if (count > 0)
 	{
-		free(result[count]);
 		count--;
+		while (count)
+		{
+			free(result[count]);
+			count--;
+		}
+		free(result[0]);
 	}
 	free(result);
 	return (NULL);
@@ -63,10 +68,9 @@ to a new array of string.
 ** @param str: the string to be splitted
 ** @param c: the delimiter
 ** @param result: the array that contains splitted string
-** @param count: size of the result array
 ** @return pointer pointing to the end of substring on str
 */
-static char	*ft_splitstr(char const *str, char c, char **result, size_t count)
+static char	*ft_splitstr(char const *str, char c, char **result)
 {
 	char		*substr;
 	char const	*end;
@@ -76,11 +80,11 @@ static char	*ft_splitstr(char const *str, char c, char **result, size_t count)
 		end++;
 	substr = (char *) malloc((end - str + 1) * sizeof(char));
 	if (!substr)
-		return (ft_deep_free(result, count));
+		return (NULL);
+	substr[end - str] = 0;
 	*result = substr;
 	if (!ft_memmove(substr, str, end - str))
-		return (ft_deep_free(result, count));
-	substr[end - str] = 0;
+		return (NULL);
 	return ((char *) end);
 }
 
@@ -95,23 +99,22 @@ static char	*ft_splitstr(char const *str, char c, char **result, size_t count)
 char	**ft_split(char const *str, char c)
 {
 	char	**result;
-	size_t	count;
 	size_t	i;
 
 	if (!str)
 		return (NULL);
-	count = ft_count_word(str, c);
-	result = (char **) malloc((count + 1) * sizeof(char *));
+	result = malloc((ft_count_word(str, c) + 1) * sizeof(char *));
 	if (!result)
 		return (NULL);
 	i = 0;
-	while (*str && i < count)
+	while (*str)
 	{
 		if (*str != c)
 		{
-			str = ft_splitstr(str, c, result + i, i) - 1;
+			str = ft_splitstr(str, c, result + i);
 			if (!str)
-				return (NULL);
+				return (ft_deep_free(result, i));
+			str--;
 			i++;
 		}
 		str++;
