cee115f929b18ebe9bc716e156829a9fe3e97a14
Anthony Tsang <anthonytsang0226@gmail.com>
2023-06-13 13:36:54 +0200

    chore: change printf header file name

--- include/LIBFT/printf.h
+++ include/LIBFT/printf.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   printf.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 15:39:24 by htsang            #+#    #+#             */
-/*   Updated: 2023/06/13 04:51:14 by htsang           ###   ########.fr       */
+/*   Updated: 2023/06/13 13:35:54 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#ifndef PRINTF_H
+# define PRINTF_H
 
 # include <stdlib.h>
 # include <unistd.h>
