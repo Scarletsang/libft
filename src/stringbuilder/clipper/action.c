c3da5a61129ef7aaeb7308395119b085f9cdab1a
Anthony Tsang <anthonytsang0226@gmail.com>
2023-06-17 17:01:13 +0200

    feat(strinbuilder/clipper): Can output slice from clipper

---
+++ src/stringbuilder/clipper/action.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   action.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/16 22:53:54 by htsang            #+#    #+#             */
+/*   Updated: 2023/06/17 15:13:19 by htsang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "LIBFT/stringbuilder/clipper.h"
+
+int	ft_sb_clipper_move_lbound(struct s_ft_sb_clipper *clipper)
+{
+	if ((clipper->lbound + 1) >= clipper->rbound)
+		return (EXIT_FAILURE);
+	clipper->lbound++;
+	return (EXIT_SUCCESS);
+}
+
+int	ft_sb_clipper_move_rbound(struct s_ft_sb_clipper *clipper)
+{
+	if (clipper->rbound <= (clipper->lbound + 1))
+		return (EXIT_FAILURE);
+	clipper->rbound--;
+	return (EXIT_SUCCESS);
+}
+
+char	*ft_sb_clipper_run(const struct s_ft_sb_clipper *clipper)
+{
+	return (ft_string_slice_to_cstring(ft_sb_clipper_slice(clipper)));
+}
+
+t_ft_string_slice	ft_sb_clipper_slice(\
+const struct s_ft_sb_clipper *clipper)
+{
+	return ((t_ft_string_slice){
+		.content = clipper->sb->buffer + clipper->lbound,
+		.size = clipper->rbound - clipper->lbound + 1
+	});
+}
--- src/stringbuilder/clipper/clipper.c
+++ src/stringbuilder/clipper/clipper.c
@@ -6,7 +6,7 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 21:10:42 by anthonytsan       #+#    #+#             */
-/*   Updated: 2023/06/13 15:26:39 by htsang           ###   ########.fr       */
+/*   Updated: 2023/06/17 15:13:10 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,41 +21,12 @@ void	ft_sb_clipper_init(struct s_ft_sb_clipper *clipper, const t_ft_sb *sb)
 	clipper->rbound = sb->size - 1;
 }
 
-int	ft_sb_clipper_move_lbound(struct s_ft_sb_clipper *clipper)
+char	ft_sb_clipper_at_lbound(const struct s_ft_sb_clipper *clipper)
 {
-	if ((clipper->lbound + 1) >= clipper->rbound)
-		return (EXIT_FAILURE);
-	clipper->lbound++;
-	return (EXIT_SUCCESS);
+	return (ft_sb_get(clipper->sb, clipper->lbound));
 }
 
-int	ft_sb_clipper_move_rbound(struct s_ft_sb_clipper *clipper)
+char	ft_sb_clipper_at_rbound(const struct s_ft_sb_clipper *clipper)
 {
-	if (clipper->rbound <= (clipper->lbound + 1))
-		return (EXIT_FAILURE);
-	clipper->rbound--;
-	return (EXIT_SUCCESS);
-}
-
-char	*ft_sb_clipper_run(const struct s_ft_sb_clipper *clipper)
-{
-	char				*result;
-	t_ft_sb_iterator	it;
-	size_t				i;
-
-	result = malloc(sizeof(char) * (clipper->rbound - \
-		clipper->lbound + 2));
-	if (!result)
-		return (NULL);
-	ft_sb_iterator_init(&it, clipper->sb);
-	it.index = clipper->lbound;
-	i = 0;
-	while (it.index <= clipper->rbound)
-	{
-		result[i] = ft_sb_iterator_current(&it);
-		i++;
-		ft_sb_iterator_next(&it);
-	}
-	result[i] = '\0';
-	return (result);
+	return (ft_sb_get(clipper->sb, clipper->rbound));
 }
