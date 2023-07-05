535c970f492fa5a41250f13463589daab0e7ca6b
Anthony Tsang <anthonytsang0226@gmail.com>
2023-07-05 11:02:05 +0200

    spec(strinbuilder): Add tests for stringbuilder iterator

--- tests/stringbuilder/stringbuilder_identity.cpp
+++ tests/stringbuilder/stringbuilder_identity.cpp
@@ -6,7 +6,7 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/02 10:56:58 by htsang            #+#    #+#             */
-/*   Updated: 2023/07/03 12:52:51 by htsang           ###   ########.fr       */
+/*   Updated: 2023/07/05 11:00:34 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,10 +55,76 @@ TEST_P(SbTest, ft_sb_delete_FromLastCharOfOutboundSize)
 	ASSERT_EQ(ret, EXIT_SUCCESS);
 }
 
+TEST_P(SbTest, ft_sb_iterator_Forwards)
+{
+	const char				*input = GetParam();
+	t_ft_sb_iterator		it;
+
+	setSb(input);
+	ft_sb_iterator_begin(&it, &sb);
+	while (!it.is_end)
+	{
+		ASSERT_EQ(ft_sb_iterator_current(&it), *input);
+		ft_sb_iterator_next(&it);
+		if (!it.is_end)
+			input++;
+	}
+	ASSERT_EQ(ft_sb_iterator_current(&it), *input);
+}
+
+TEST_P(SbTest, ft_sb_iterator_Backwards)
+{
+	const char				*input = GetParam();
+	size_t					input_size = strlen(input);
+	t_ft_sb_iterator		it;
+
+	setSb(input);
+	ft_sb_iterator_end(&it, &sb);
+	while (!it.is_end)
+	{
+		ASSERT_EQ(ft_sb_iterator_current(&it), input[input_size - 1]);
+		ft_sb_iterator_prev(&it);
+		if (!it.is_end)
+			input_size--;
+	}
+	ASSERT_EQ(ft_sb_iterator_current(&it), input[input_size - 1]);
+}
+
+TEST_P(SbTest, ft_sb_iterator_ForwardsThenBackwards)
+{
+	const char				*input = GetParam();
+	size_t					i;
+	t_ft_sb_iterator		it;
+
+	setSb(input);
+	ft_sb_iterator_begin(&it, &sb);
+	i = 0;
+	while (!it.is_end)
+	{
+		ASSERT_EQ(ft_sb_iterator_current(&it), input[i]);
+		ft_sb_iterator_next(&it);
+		if (!it.is_end)
+			i++;
+	}
+	ASSERT_EQ(ft_sb_iterator_current(&it), input[i]);
+	ft_sb_iterator_prev(&it);
+	if (i > 0)
+		i--;
+	while (!it.is_end)
+	{
+		ASSERT_EQ(ft_sb_iterator_current(&it), input[i]);
+		ft_sb_iterator_prev(&it);
+		if (!it.is_end)
+			i--;
+	}
+	ASSERT_EQ(ft_sb_iterator_current(&it), input[i]);
+}
+
 INSTANTIATE_TEST_SUITE_P(Sb, SbTest, testing::Values(
 	"",
 	"Hello World",
 	"1\02\0",
 	"qwertyuiopasdfghjklzxcvbnm",
+	"wertyuiopasdfghjklz",
 	"\t\n\v\f\r"
 ));
