fc7845fef2b5b2d33ba2a405dfc5e3984a76784a
Anthony Tsang <anthonytsang0226@gmail.com>
2023-08-04 14:52:55 +0200

    chore(parser): Update tests to the newest version of libft parser

--- tests/parser/parser.cpp
+++ tests/parser/parser.cpp
@@ -6,11 +6,12 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 21:50:24 by htsang            #+#    #+#             */
-/*   Updated: 2023/08/04 04:10:04 by htsang           ###   ########.fr       */
+/*   Updated: 2023/08/04 14:47:13 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser_test.hpp"
+#include <string>
 
 TEST(ParserTest, parser_char)
 {
@@ -33,7 +34,7 @@ TEST(ParserTest, parse_number)
   t_ft_str                  input = ft_str_from_cstring("-123090");
   struct s_ft_parser_atom entity = ft_parser_atom_empty(input, true);
 
-  entity = parse_number(entity, {});
+  entity = ft_parser_int(entity, {});
   EXPECT_EQ(entity.payload.as_int, -123090);
   EXPECT_STREQ((char *) entity.string.content, NULL);
 }
@@ -46,4 +47,16 @@ TEST(ParserTest, parse_expr)
   entity = parse_expr(entity, {});
   EXPECT_EQ(entity.payload.as_int, -2827);
   EXPECT_STREQ((char *) entity.string.content, NULL);
+}
+
+TEST(ParserTest, parse_expr2)
+{
+  std::string               string;
+  std::getline(std::cin, string);
+  t_ft_str                  input = ft_str_from_cstring(string.c_str());
+  struct s_ft_parser_atom entity = ft_parser_atom_empty(input, true);
+
+  entity = parse_expr(entity, {});
+  printf("result: %d\n", entity.payload.as_int);
+  printf("left  : '%s'\n", (char *) entity.string.content);
 }
--- tests/parser/setup.cpp
+++ tests/parser/setup.cpp
@@ -6,7 +6,7 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 23:22:24 by htsang            #+#    #+#             */
-/*   Updated: 2023/08/04 04:26:34 by htsang           ###   ########.fr       */
+/*   Updated: 2023/08/04 14:51:09 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ union u_ft_tobject option)
 {
 	(void) option;
   return (ft_parser_or((struct s_ft_parser_entity[2]){
-      ft_parser_entity(parse_scalar_sentence, {}),
-      ft_parser_entity(parse_term, {})
+      ft_parser_entity(parse_scalar_sentence, ft_tobject_empty()),
+      ft_parser_entity(parse_term, ft_tobject_empty())
     }, 2, input));
 }
 
@@ -33,8 +33,8 @@ union u_ft_tobject option)
 {
   (void) option;
   return (ft_parser_or((struct s_ft_parser_entity[2]){
-      ft_parser_entity(parse_vector_sentence, {}),
-      ft_parser_entity(parse_factor, {})
+      ft_parser_entity(parse_vector_sentence, ft_tobject_empty()),
+      ft_parser_entity(parse_factor, ft_tobject_empty())
     }, 2, input));
 }
 
@@ -45,22 +45,11 @@ union u_ft_tobject option)
   return (ft_parser_or((struct s_ft_parser_entity[2]){
       ft_combinator_entity(ft_parser_and, (struct s_ft_parser_entity[3]) {
         ft_parser_entity(ft_parser_ignore, ft_tobject_str((char *) "(")),
-        ft_parser_entity(parse_expr, {}),
+        ft_parser_entity(parse_expr, ft_tobject_empty()),
         ft_parser_entity(ft_parser_ignore, ft_tobject_str((char *) ")"))
       }, 3),
-      ft_parser_entity(parse_number, {})
+      ft_parser_entity(ft_parser_int, ft_tobject_empty())
     }, 2, input));
-}
-
-struct s_ft_parser_atom parse_parantesis(struct s_ft_parser_atom input, \
-union u_ft_tobject option)
-{
-  (void) option;
-  return (ft_parser_and((struct s_ft_parser_entity[3]) {
-      ft_parser_entity(ft_parser_ignore, ft_tobject_str((char *) "(")),
-      ft_parser_entity(parse_expr, {}),
-      ft_parser_entity(ft_parser_ignore, ft_tobject_str((char *) ")"))
-    }, 3, ft_parser_atom_payload_set(input, ft_tobject_int(0))));
 }
 
 struct s_ft_parser_atom parse_scalar_sentence(struct s_ft_parser_atom input, \
@@ -115,41 +104,4 @@ union u_ft_tobject option)
   else if (op == '/')
     number1 /= number2;
   return (ft_parser_atom_payload_set(entity, ft_tobject_int(number1)));
-}
-
-struct s_ft_parser_atom parse_int(struct s_ft_parser_atom input, \
-union u_ft_tobject is_negative)
-{
-  char  *c;
-
-  c = ft_parser_peek(input.string, 0);
-  if (!c || !isdigit(*c))
-    return (ft_parser_atom_empty(input.string, false));
-  else if (is_negative.as_bool)
-  {
-    if (input.payload.as_int < (INT_MIN + (*c - '0')) / 10)
-      return (ft_parser_atom_empty(input.string, false));
-    input.payload.as_int = input.payload.as_int * 10 - (*c - '0');
-  }
-  else
-  {
-    if (input.payload.as_int > (INT_MAX - (*c - '0')) / 10)
-      return (ft_parser_atom_empty(input.string, false));
-    input.payload.as_int = input.payload.as_int * 10 + (*c - '0');
-  }
-	return (ft_parser_atom(input.payload, ft_parser_advance(input.string, 1)));
-}
-
-struct s_ft_parser_atom parse_number(struct s_ft_parser_atom input, \
-union u_ft_tobject option)
-{
-	bool							        is_negative;
-
-  (void) option;
-	input = ft_parser_char(input, ft_tobject_str((char *) "-"));
-	is_negative = ft_parser_atom_is_ok(input);
-  input = ft_parser_atom_payload_set(input, ft_tobject_int(0));
-  input.is_valid = true;
-  return (ft_parser_accumulate(\
-    ft_parser_entity(&parse_int, ft_tobject_bool(is_negative)), input));
 }
